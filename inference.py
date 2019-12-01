493b5c071615749ff71283808f069331683b8179
Zack Zukowski <drawordoodle@gmail.com>
2019-12-01 13:39:16 -0800

    add paths as arguments pep 8 var names lowercase

--- inference.py
+++ inference.py
@@ -5,27 +5,30 @@ from network.model import Generator
 from webcam_demo.webcam_extraction_conversion import generate_landmarks
 import os.path
 
-def main(path_to_model_weights = 'model_weights.tar', path_to_embedding = 'e_hat_video.tar', output_dir='./'):
+
+def main(model_weights_path: str = 'model_weights.tar',
+         embedding_path: str = 'e_hat_video.tar',
+         video_path: str = 'examples/fine_tuning/test_video.mp4',
+         output_dir: str = './'):
     """Init"""
     device = torch.device("cuda:0")
     cpu = torch.device("cpu")
 
-    checkpoint = torch.load(path_to_model_weights, map_location=cpu)
-    e_hat = torch.load(path_to_embedding, map_location=cpu)
+    checkpoint = torch.load(model_weights_path, map_location=cpu)
+    e_hat = torch.load(embedding_path, map_location=cpu)
     e_hat = e_hat['e_hat'].to(device)
 
-    G = Generator(256, finetuning=True, e_finetuning=e_hat)
-    G.eval()
+    generator = Generator(256, finetuning=True, e_finetuning=e_hat)
+    generator.eval()
 
     """Training Init"""
-    G.load_state_dict(checkpoint['G_state_dict'])
-    G.to(device)
-    G.finetuning_init()
-
+    generator.load_state_dict(checkpoint['G_state_dict'])
+    generator.to(device)
+    generator.finetuning_init()
 
     """Main"""
     print('PRESS Q TO EXIT')
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(video_path)
 
     with torch.no_grad():
         while True:
@@ -34,37 +37,36 @@ def main(path_to_model_weights = 'model_weights.tar', path_to_embedding = 'e_hat
             g_y = g_y.unsqueeze(0)
             x = x.unsqueeze(0)
 
-
-            #forward
+            # forward
             # Calculate average encoding vector for video
-            #f_lm_compact = f_lm.view(-1, f_lm.shape[-4], f_lm.shape[-3], f_lm.shape[-2], f_lm.shape[-1]) #BxK,2,3,224,224
-            #train G
+            # f_lm_compact = f_lm.view(-1, f_lm.shape[-4], f_lm.shape[-3], f_lm.shape[-2], f_lm.shape[-1]) #BxK,2,3,224,224
+            # train generator
 
-            x_hat = G(g_y, e_hat)
+            x_hat = generator(g_y, e_hat)
 
             plt.clf()
-            out1 = x_hat.transpose(1,3)[0]/255
-            #for img_no in range(1,x_hat.shape[0]):
+            out1 = x_hat.transpose(1, 3)[0] / 255
+            # for img_no in range(1,x_hat.shape[0]):
             #    out1 = torch.cat((out1, x_hat.transpose(1,3)[img_no]), dim = 1)
             out1 = out1.to(cpu).numpy()
-            #plt.imshow(out1)
-            #plt.show()
+            # plt.imshow(out1)
+            # plt.show()
 
-            #plt.clf()
-            out2 = x.transpose(1,3)[0]/255
-            #for img_no in range(1,x.shape[0]):
+            # plt.clf()
+            out2 = x.transpose(1, 3)[0] / 255
+            # for img_no in range(1,x.shape[0]):
             #    out2 = torch.cat((out2, x.transpose(1,3)[img_no]), dim = 1)
             out2 = out2.to(cpu).numpy()
-            #plt.imshow(out2)
-            #plt.show()
+            # plt.imshow(out2)
+            # plt.show()
 
-            #plt.clf()
-            out3 = g_y.transpose(1,3)[0]/255
-            #for img_no in range(1,g_y.shape[0]):
+            # plt.clf()
+            out3 = g_y.transpose(1, 3)[0] / 255
+            # for img_no in range(1,g_y.shape[0]):
             #    out3 = torch.cat((out3, g_y.transpose(1,3)[img_no]), dim = 1)
             out3 = out3.to(cpu).numpy()
-            #plt.imshow(out3)
-            #plt.show()
+            # plt.imshow(out3)
+            # plt.show()
 
             cv2.imwrite(os.path.join(output_dir, 'fake.png'), cv2.cvtColor(out1, cv2.COLOR_BGR2RGB))
             cv2.imwrite(os.path.join(output_dir, 'head_track.png'), cv2.cvtColor(out2, cv2.COLOR_BGR2RGB))
@@ -73,6 +75,7 @@ def main(path_to_model_weights = 'model_weights.tar', path_to_embedding = 'e_hat
                 break
     cap.release()
     cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     main()
