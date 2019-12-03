d6bb533ca15f26fd264f7057810c9b827b017720
Zack Zukowski <drawordoodle@gmail.com>
2019-12-02 23:39:31 -0800

    plt imsave for multi image

--- inference.py
+++ inference.py
@@ -48,32 +48,29 @@ def main(model_weights_path: str = 'model_weights.tar',
 
             plt.clf()
             out1 = x_hat.transpose(1, 3)[0] / 255
-            # for img_no in range(1,x_hat.shape[0]):
-            #    out1 = torch.cat((out1, x_hat.transpose(1,3)[img_no]), dim = 1)
+            for img_no in range(1,x_hat.shape[0]):
+               out1 = torch.cat((out1, x_hat.transpose(1,3)[img_no]), dim = 1)
             out1 = out1.to(cpu).numpy()
-            # plt.imshow(out1)
-            # plt.show()
-
-            # plt.clf()
-            out2 = x.transpose(1, 3)[0] / 255
-            # for img_no in range(1,x.shape[0]):
-            #    out2 = torch.cat((out2, x.transpose(1,3)[img_no]), dim = 1)
-            out2 = out2.to(cpu).numpy()
-            # plt.imshow(out2)
-            # plt.show()
-
-            # plt.clf()
-            out3 = g_y.transpose(1, 3)[0] / 255
-            # for img_no in range(1,g_y.shape[0]):
-            #    out3 = torch.cat((out3, g_y.transpose(1,3)[img_no]), dim = 1)
-            out3 = out3.to(cpu).numpy()
             plt.imshow(out1)
+            plt.show()
+            plt.imsave(os.path.join(output_dir, 'fake-{}.png'.format(enum)), out1)
+            plt.clf()
+            out2 = x.transpose(1, 3)[0] / 255
+            for img_no in range(1,x.shape[0]):
+               out2 = torch.cat((out2, x.transpose(1,3)[img_no]), dim = 1)
+            out2 = out2.to(cpu).numpy()
             plt.imshow(out2)
+            plt.show()
+            plt.imsave(os.path.join(output_dir, 'head_track-{}.png'.format(enum)), out2)
+            plt.clf()
+            out3 = g_y.transpose(1, 3)[0] / 255
+            for img_no in range(1,g_y.shape[0]):
+               out3 = torch.cat((out3, g_y.transpose(1,3)[img_no]), dim = 1)
+            out3 = out3.to(cpu).numpy()
             plt.imshow(out3)
-
-            cv2.imwrite(os.path.join(output_dir, 'fake-{}.png'.format(enum)), out1)
-            cv2.imwrite(os.path.join(output_dir, 'head_track-{}.png'.format(enum)), out2)
-            cv2.imwrite(os.path.join(output_dir, 'landmark-{}.png'.format(enum)), out3)
+            plt.show()
+            plt.imsave(os.path.join(output_dir, 'landmark-{}.png'.format(enum)), out3)
+            plt.clf()
             if cv2.waitKey(1) == ord('q'):
                 break
     cap.release()
